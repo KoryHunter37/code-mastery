ba9bb90d2a6fc86ec5421be7655e20f4a32a9ab0
Kory Hunter <koryhunter@gatech.edu>
2018-10-16 19:29:07 -0400

    ascii

---
+++ playground/built-in-functions/ascii/ascii.py
@@ -0,0 +1,13 @@
+﻿# ascii(object)
+
+# Return a string (printable representation) of an object.
+# Convert non-ASCII characters into escaped characters.
+# Uses repr() to find the printable representation.
+
+# > \U0001f525
+# Converts the fire emoji, as it is non-ASCII.
+print(ascii("🔥"))
+
+# > The price for one serving is \xa5500.
+# Converts the yen symbol, but leaves the rest of the string intact.
+print(ascii("The price for one serving is ¥500."))
