2f35b7168d7e7d5ac013d36bf13d59ac70dad001
Kory Hunter <koryhunter@gatech.edu>
2018-10-16 16:33:18 -0400

    abs

---
+++ playground/built-in-functions/abs/abs.py
@@ -0,0 +1,18 @@
+# abs(x)
+
+# Return the absolute value of a number. 
+# The argument may be an integer or a floating point number.
+
+# > 5
+print(abs(-5))
+
+# > 5
+print(abs(5))
+
+# > 2 2
+# Regardless of order, will always print the "difference" between two numbers. 
+print(abs(7 - 9), abs(9-7))
+
+# > 4 4
+# Can find the "difference" between negative and positive numbers.
+print(abs((-2) - 2), abs(2 - (-2)))
