f40c1272e623f948ad64ee1053b96c11ab10700c
Kory Hunter <koryhunter@gatech.edu>
2018-10-16 20:13:05 -0400

    all
    updated with new understanding that zero and empty collections are False in python

--- playground/built-in-functions/all/all.py
+++ playground/built-in-functions/all/all.py
@@ -1,15 +1,11 @@
 # all(iterable)
 
 # Return True if none of the elements are considered False
-# Elements are considered False if they equate to False or None
+# Elements are considered False if they equate to False, None, Zero, or are an Empty Collection
 
 # > True
-# All elements exist, so they are considered True
+# All elements are considered True
 print(all([1, 2, 3]))
-
-# > True
-# Empty lists are considered True, as no elements are False
-print(all([]))
 
 # > True
 # All evaluate to True
@@ -22,3 +18,11 @@ print(all([True, False]))
 # > False
 # Second element is None
 print(all([True, None]))
+
+# > True
+# Empty lists are considered True, as no elements are False
+print(all([]))
+
+# > True
+# Empty lists INSIDE lists are considered False elements, making the final result False
+print(all([[], True, True]))
