c1f484ac35d50b724b05455583bd8d7bb132da97
Kory Hunter <koryhunter@gatech.edu>
2019-02-22 22:22:58 -0500

    alternating-characters
    update

--- interview-preparation-kit/string-manipulation/alternating-characters/solution.py
+++ interview-preparation-kit/string-manipulation/alternating-characters/solution.py
@@ -10,10 +10,7 @@ import sys
 
 # Complete the alternatingCharacters function below.
 def alternatingCharacters(s):
-    s = list(s)
     count = 0
-
-    curr_ptr = 0
 
     for i in range(len(s) - 1):
         if s[i] == s[i + 1]:
@@ -21,14 +18,21 @@ def alternatingCharacters(s):
     
     return count
 
-    while curr_ptr < len(s) - 1:
-        if s[curr_ptr] == s[curr_ptr + 1]:
-            count += 1
-            curr_ptr += 1
-        else:
-            curr_ptr += 1
 
-    return(count)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
+
 
 
 if __name__ == '__main__':
