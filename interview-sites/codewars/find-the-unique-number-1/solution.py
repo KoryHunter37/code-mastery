34146f50643125a5747190ec3970d1d081a3f48c
Kory Hunter <koryhunter@gatech.edu>
2019-03-01 23:14:16 -0500

    find-the-unique-number-1
    improved version

--- interview-sites/codewars/find-the-unique-number-1/solution.py
+++ interview-sites/codewars/find-the-unique-number-1/solution.py
@@ -1,15 +1,5 @@
 # https://www.codewars.com/kata/find-the-unique-number-1/train/python
 
 def find_uniq(arr):
-    nums = []
-    
-    for number in arr:
-        if number in nums:
-            main_num = number
-            break
-        else:
-            nums.append(number)
-            
-    arr = set(arr)
-    arr.remove(main_num)
-    return arr.pop()
+    a, b = set(arr)
+    return a if arr.count(a) < arr.count(b) else b
