b8098ff745e387c4c0cdd40eb82fe5b2f1ffe707
Kory Hunter <koryhunter@gatech.edu>
2019-03-01 19:57:37 -0500

    vowel-count

--- interview-sites/codewars/jaden-casing-strings/solution.py
+++ interview-sites/codewars/jaden-casing-strings/solution.py
@@ -1,4 +1,4 @@
-# https://www.codewars.com/kata/5390bac347d09b7da40006f6/solutions/python/
+# https://www.codewars.com/kata/5390bac347d09b7da40006f6/solutions/python
 
 from string import capwords
 
---
+++ interview-sites/codewars/vowel-count/solution.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/python
+
+def getCount(inputStr):
+    num_vowels = 0
+    
+    for letter in inputStr:
+        if letter in ["a", "e", "i", "o", "u"]:
+            num_vowels += 1
+    
+    return num_vowels
