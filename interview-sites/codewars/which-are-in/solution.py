ddf3b1a17180fbee084de106584d5fe1c1b18134
Kory Hunter <koryhunter@gatech.edu>
2019-03-01 22:49:07 -0500

    which-are-in
    improved version

---
+++ interview-sites/codewars/which-are-in/solution.py
@@ -0,0 +1,4 @@
+# https://www.codewars.com/kata/which-are-in/train/python
+
+def in_array(array1, array2):          
+    return sorted(set([word for word in array1 if any(word in other_word for other_word in array2)]))
