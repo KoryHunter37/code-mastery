967759df21d20e9a9e783481d696820dd1b2c114
Kory Hunter <koryhunter@gatech.edu>
2019-03-01 22:39:59 -0500

    are-they-the-same

--- interview-sites/codewars/are-they-the-same/solution.py
+++ interview-sites/codewars/are-they-the-same/solution.py
@@ -1,16 +1,8 @@
 # https://www.codewars.com/kata/are-they-the-same/train/python
 
 def comp(array1, array2):
-    
-    # Check for None inputs
-    if array1 is None or array2 is None:
+    try:    
+        array1, array2 = sorted(array1), sorted(array2)
+        return all( [array1[i] ** 2 == array2[i] for i in range(len(array1))] )
+    except:
         return False
-       
-    # Check for uneven inputs
-    if len(array1) != len(array2):
-        return False
-        
-    array1, array2 = sorted(array1), sorted(array2)
-    
-    
-    return all( [array1[i] ** 2 == array2[i] for i in range(len(array1))] )
