ef30aec5276dfda7b7bc4068cb2b8fe1b6ce5719
Kory Hunter <koryhunter@gatech.edu>
2019-03-09 16:23:33 -0500

    calculating-with-functions

--- interview-sites/codewars/calculating-with-functions/solution.py
+++ interview-sites/codewars/calculating-with-functions/solution.py
@@ -3,49 +3,49 @@
 def zero(n=None):
     if n is None:    
         return 0
-    return n[0](0, n[1])
+    return n(0)
 def one(n=None):
     if n is None:    
         return 1
-    return n[0](1, n[1])
+    return n(1)
 def two(n=None):
     if n is None:    
         return 2
-    return n[0](2, n[1]) 
+    return n(2) 
 def three(n=None):
     if n is None:    
         return 3
-    return n[0](3, n[1])
+    return n(3)
 def four(n=None):
     if n is None:    
         return 4
-    return n[0](4, n[1])
+    return n(4)
 def five(n=None):
     if n is None:    
         return 5
-    return n[0](5, n[1])
+    return n(5)
 def six(n=None):
     if n is None:    
         return 6
-    return n[0](6, n[1])
+    return n(6)
 def seven(n=None):
     if n is None:   
         return 7
-    return n[0](7, n[1])
+    return n(7)
 def eight(n=None):
     if n is None:    
         return 8
-    return n[0](8, n[1])
+    return n(8)
 def nine(n=None):
     if n is None:    
         return 9
-    return n[0](9, n[1])
+    return n(9)
 
-def plus(n):
-    return (lambda n1, n2: n1 + n2, n)
-def minus(n):
-    return (lambda n1, n2: n1 - n2, n)
-def times(n):
-    return (lambda n1, n2: n1 * n2, n)
-def divided_by(n):
-    return (lambda n1, n2: n1 // n2, n)
+def plus(n2):
+    return (lambda n1: n1 + n2)
+def minus(n2):
+    return (lambda n1: n1 - n2)
+def times(n2):
+    return (lambda n1: n1 * n2)
+def divided_by(n2):
+    return (lambda n1: n1 // n2)
