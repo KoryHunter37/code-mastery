7894dda6384f9b70d26b225a8378047513acfe83
Kory Hunter <koryhunter@gatech.edu>
2019-03-19 19:34:25 -0400

    even-digits-only
    improved

--- interview-sites/codesignal/intro/level-6/even-digits-only/solution.py
+++ interview-sites/codesignal/intro/level-6/even-digits-only/solution.py
@@ -1,4 +1,4 @@
 # https://app.codesignal.com/arcade/intro/level-6/6cmcmszJQr6GQzRwW
 
 def evenDigitsOnly(n):
-    return all( True if int(i) % 2 == 0 else False for i in str(n) )
+    return all( int(i) % 2 == 0 for i in str(n) )
