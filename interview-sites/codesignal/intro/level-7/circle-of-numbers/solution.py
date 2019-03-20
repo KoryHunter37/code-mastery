8dcf6a4fac9060262c6227800dd5feb1c8224b5f
Kory Hunter <koryhunter@gatech.edu>
2019-03-19 20:37:04 -0400

    circle-of-numbers

--- interview-sites/codesignal/intro/level-7/circle-of-numbers/solution.py
+++ interview-sites/codesignal/intro/level-7/circle-of-numbers/solution.py
@@ -1,6 +1,4 @@
 # https://app.codesignal.com/arcade/intro/level-7/vExYvcGnFsEYSt8nQ
 
-from math import floor
-
 def circleOfNumbers(n, firstNumber):
-    return math.floor((firstNumber + n/2) % n)
+    return (firstNumber + n//2) % n
