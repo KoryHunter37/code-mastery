9b6938a52873d778bd0a0157feffdd70aa518618
Kory Hunter <koryhunter@gatech.edu>
2019-03-23 23:32:10 -0400

    late-ride

---
+++ interview-sites/codesignal/the-core/level-1/late-ride/solution.py
@@ -0,0 +1,5 @@
+# https://app.codesignal.com/arcade/code-arcade/intro-gates/aiKck9MwwAKyF8D4L
+
+def lateRide(n):
+    hours, minutes = map(str, divmod(n, 60))
+    return sum(int(i) for i in hours + minutes)
