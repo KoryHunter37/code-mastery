76c818211a280b4e5a56b2cdca1c9f0dc5b7e720
Kory Hunter <koryhunter@gatech.edu>
2019-03-24 11:15:07 -0400

    unique-email-addresses
    improved

--- interview-sites/leetcode/unique-email-addresses/solution.py
+++ interview-sites/leetcode/unique-email-addresses/solution.py
@@ -1,21 +1,13 @@
 # https://leetcode.com/problems/unique-email-addresses
 
-from string import ascii_lowercase, digits
-
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
         for i, email in enumerate(emails):
             new_email = []
             local, domain = email.split('@')
-            for letter in local:                        
-                if letter in ascii_lowercase or letter in digits:
-                    new_email.append(letter)
-                elif letter == '+':
-                    break
-                    
-            new_email.extend(domain) 
-            
-            emails[i] = ''.join(new_email)
+            local = local.split('+')[0]
+            local = local.replace('.', '')
+
+            emails[i] = local + domain
 
         return len(set(emails))
-                    
