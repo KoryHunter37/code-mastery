227f83efae646a7beac7356d85b91273e027b265
Kory Hunter <koryhunter@gatech.edu>
2019-03-23 23:47:32 -0400

    phone-call
    improved

--- interview-sites/codesignal/the-core/level-1/phone-call/solution.py
+++ interview-sites/codesignal/the-core/level-1/phone-call/solution.py
@@ -1,13 +1,13 @@
 # https://app.codesignal.com/arcade/code-arcade/intro-gates/mZAucMXhNMmT7JWta
 
 def phoneCall(min1: int, min2_10: int, min11: int, s: int) -> int:
-    cost = {0: min1, 1: min2_10, 2: min2_10, 3: min2_10, 4: min2_10, 5: min2_10, 6: min2_10, 7: min2_10, 8: min2_10, 9: min2_10}
-    minutes = 0
     
-    while s > 0:
-        s -= cost.get(minutes, min11)
-        if s >= 0:
-            minutes += 1
-        
-    return minutes
-        
+    if s < min1:
+        return 0
+    elif s == min1:
+        return 1
+    elif min2_10 <= s - min1 <= min2_10 * 9:
+        return (s - min1) // min2_10 + 1
+    else:
+        return (s - min1 - min2_10 * 9) // min11 + 10
+     
