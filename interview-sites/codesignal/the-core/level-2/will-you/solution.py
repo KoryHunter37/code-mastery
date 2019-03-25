8a532e9b9bd235da119f13bcf146df89e5fd8786
Kory Hunter <koryhunter@gatech.edu>
2019-03-25 17:33:59 -0400

    will-you

---
+++ interview-sites/codesignal/the-core/level-2/will-you/solution.py
@@ -0,0 +1,4 @@
+# https://app.codesignal.com/arcade/code-arcade/at-the-crossroads/jZ4ZSiGohzFTeg4yb
+
+def willYou(young, beautiful, loved):
+    return young and beautiful and not loved or ((not young or not beautiful) and loved)
