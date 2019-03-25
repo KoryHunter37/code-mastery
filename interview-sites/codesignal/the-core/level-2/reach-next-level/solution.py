d992faef3c005ffae7a2c637a3205aaf370566e7
Kory Hunter <koryhunter@gatech.edu>
2019-03-25 16:40:04 -0400

    reach-next-level

---
+++ interview-sites/codesignal/the-core/level-2/reach-next-level/solution.py
@@ -0,0 +1,4 @@
+# https://app.codesignal.com/arcade/code-arcade/at-the-crossroads/m9wjpkCjgofg7gs8N
+
+def reachNextLevel(experience: int, threshold: int, reward: int) -> bool:
+    return experience + reward >= threshold
