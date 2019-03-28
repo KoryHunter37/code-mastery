b073c1610c5a648b4f352b1fa093690b37385203
Kory Hunter <koryhunter@gatech.edu>
2019-03-28 19:54:21 -0400

    concatenate-arrays

---
+++ interview-sites/codesignal/the-core/level-5/concatenate-arrays/solution.py
@@ -0,0 +1,6 @@
+# https://app.codesignal.com/arcade/code-arcade/list-forest-edge/GeqSK26bvdrarkGH9
+
+from typing import List
+
+def concatenateArrays(a: List[int], b: List[int]) -> List[int]:
+    return a + b
