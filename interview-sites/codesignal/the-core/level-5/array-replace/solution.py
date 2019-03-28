fd71ca7f23060a9ba53b4db3182575e52b3a269c
Kory Hunter <koryhunter@gatech.edu>
2019-03-28 19:42:08 -0400

    array-replace

---
+++ interview-sites/codesignal/the-core/level-5/array-replace/solution.py
@@ -0,0 +1,4 @@
+# https://app.codesignal.com/arcade/code-arcade/list-forest-edge/mCkmbxdMsMTjBc3Bm
+
+def arrayReplace(inputArray, elemToReplace, substitutionElem):
+    return [i if i != elemToReplace else substitutionElem for i in inputArray]
