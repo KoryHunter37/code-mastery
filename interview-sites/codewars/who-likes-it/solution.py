279a6d88a1fc1118eea2d1ef2aaee876264c0d9f
Kory Hunter <kouriiraiko@gmail.com>
2019-06-07 17:24:24 -0400

    who-likes-it

---
+++ interview-sites/codewars/who-likes-it/solution.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/who-likes-it/train/python
+
+def likes(names):
+    if len(names) == 0:
+        return 'no one likes this'
+        
+    if len(names) == 1:
+        return f'{names[0]} likes this'
+        
+    if len(names) == 2:
+        return f'{names[0]} and {names[1]} like this'
+        
+    if len(names) == 3:
+        return f'{names[0]}, {names[1]} and {names[2]} like this'
+        
+    if len(names) > 3:
+        return f'{names[0]}, {names[1]} and {len(names) - 2} others like this'
